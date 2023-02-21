e111bcc26d8b46626029e8e843015c6b3125471b
shrikiran <shrikiran@CLAYFIN-LP263>
2023-02-21 12:49:08 +0530

    added common api url for 5 rest controllers

--- src/main/java/com/ims/controller/FeedBackController.java
+++ src/main/java/com/ims/controller/FeedBackController.java
@@ -1,10 +1,12 @@
 package com.ims.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin
+@RequestMapping("/ims/feedback")
 public class FeedBackController {
 
 }
