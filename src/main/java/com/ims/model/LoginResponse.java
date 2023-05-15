b84541b5762cdeea8c5852b082290b24eb1f2982
Kishore-Clayfin <110982304+Kishore-Clayfin@users.noreply.github.com>
2023-05-15 09:44:01 +0530

    Test Commit

--- src/main/java/com/ims/model/LoginResponse.java
+++ src/main/java/com/ims/model/LoginResponse.java
@@ -3,6 +3,26 @@ package com.ims.model;
 public class LoginResponse {
 	
 	private String token;
+	private User user;
+	private String role;
+	
+	
+	
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
 
 	public String getToken() {
 		return token;
@@ -10,6 +30,11 @@ public class LoginResponse {
 
 	public void setToken(String token) {
 		this.token = token;
+	}
+
+	@Override
+	public String toString() {
+		return "LoginResponse : Token => " + token + ", User => " + user + ", Role => " + role ;
 	}
 	
 	
